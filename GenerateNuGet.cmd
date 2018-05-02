093822d1f8a06faaf8d499c2fe5086ec509e3131
NUANCE\pravin.sable <pravin.sable@nuance.com>
2018-05-01 21:57:02 -0400

    .

--- GenerateNuGet.cmd
+++ GenerateNuGet.cmd
@@ -4,114 +4,28 @@
 set path=%path%;C:\Program Files (x86)\Microsoft^ Visual Studio\2017\Professional\Common7\IDE
 set bitbucket=C:\src\BitBucket
 
+del /q "%bitbucket%\NuGetFlat\*"
+for /d %%x in ("%bitbucket%\NuGetFlat\*") do @rd /s /q "%%x"
+DEL /F /Q /S "\\127.0.0.1\NuGetPackages\*"
+del /q "\\127.0.0.1\NuGetPackages\*"
+for /d %%x in ("\\127.0.0.1\NuGetPackages\*") do @rd /s /q "%%x"
 
-pushd .
-
-:MyLoop
-cls
-taskkill /im msbuild.exe /f /t 2>NUL
-REM del /q "%bitbucket%\NuGetFlat\*"
-REM for /d %%x in ("%bitbucket%\NuGetFlat\*") do @rd /s /q "%%x"
-REM DEL /F /Q /S "\\127.0.0.1\NuGetPackages\*"
-REM del /q "\\127.0.0.1\NuGetPackages\*"
-REM for /d %%x in ("\\127.0.0.1\NuGetPackages\*") do @rd /s /q "%%x"
-
-
-IF %1.==. GOTO SkippDACPAC
-
-cd  "%bitbucket%\deployments\Database"
-set solution=Primordial.Database.sln
-call:pull
-call:compile
-call "C:\src\github\pravinsable\batchfiles\DACPAC.cmd" 
-
-:SkippDACPAC
-
-
-
-cd "%bitbucket%\primordial.all.dlls"
-git reset --hard
-git clean -fxd
-git checkout master
-git pull
 
 cd "%bitbucket%\core"
 set solution=Primordial.Core.projects.sln
 call:pull
 call:compile
 cd "%bitbucket%\core\Primordial.Core\"
-REM call:nuget
-
-
-
-set solution=Primordial.Audit.sln
-call:pull
-call:compile
+call:nuget
 
 
 cd "%bitbucket%\common\Nuance.Primordial.Common"
 set solution=Nuance.Primordial.Common.sln
 call:pull
 call:compile
-
 cd "%bitbucket%\common\Nuance.Primordial.Common\Nuance.Primordial.Common\"
-REM call:nuget
+call:nuget
 
-cd "%bitbucket%\core\Primordial.Components"
-set solution=Primordial.Components.sln
-call:pull
-call:compile
-
-
-cd "%bitbucket%\search\Primordial.Search"
-set solution=Primordial.Search.sln
-call:pull
-call:compile
-
-
-cd "%bitbucket%\core\Primordial.Components\Primordial.Documents.RtfConverter"
-set solution=Primordial.Documents.Util.sln
-call:pull
-call:compile
-
-
-cd "%bitbucket%\queryengine\QueryEngine"
-set solution=Primordial.Worklists.QueryEngine.sln
-call:pull
-call:compile
-
-
-cd "%bitbucket%\lcswebapp"
-set solution=LungScreening.sln
-git checkout -- LungScreening\Web.config
-call:pull
-sed -i "s/ServiceNode\" value=\"1/ServiceNode\" value=\"0/" LungScreening\Web.config
-sed -i "s/Data Source=10.1.10.14;/Data Source=.;/i" LungScreening\Web.config
-
-
-call:compile
-
-cd "%bitbucket%\configmanager"
-set solution=ConfigManager.sln
-call:pull
-call:compile
-
-cd "%bitbucket%\empericus"
-set solution=Primordial.HL7.Projects.sln
-call:pull
-call:compile
-
-
-popd
-
-taskkill /im msbuild.exe /f /t 2>NUL
-
-pause
-GOTO :MyLoop
-:EndLoop
-
-
-goto:eof
 
 :compile
 echo "%cd%\%solution%"
@@ -143,6 +57,7 @@ if not %branch%==master (
 )
 goto:eof
 
+
 :nuget
 REM sed -i "s#http://vdiagnugetsrv/Nuance.Diag.NuGetServer/nuget#\\127.0.0.1\NuGetPackages#" %appdata%\NuGet\NuGet.Config 
 sed -i "s/Configuration=Release/Configuration=Debug/i" "CreateNugetPackage.bat"
@@ -151,4 +66,5 @@ move "*.nupkg" "%bitbucket%\NuGetFlat"
 nuget init "%bitbucket%\NuGetFlat" \\127.0.0.1\NuGetPackages
 REM sed -i "s#\\127.0.0.1\NuGetPackages#http://vdiagnugetsrv/Nuance.Diag.NuGetServer/nuget#" %appdata%\NuGet\NuGet.Config 
 goto:eof
+
 
